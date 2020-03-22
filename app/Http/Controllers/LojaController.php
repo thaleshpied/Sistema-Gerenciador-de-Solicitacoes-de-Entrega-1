c9a478792bf4b35f3e35a68635d1d5ab0cd932f4
Caio Gabriel Nunes <caiogabriel1000@gmail.com>
2020-03-22 20:44:16 -0300

    Adicionando controlador da loja

--- app/Http/Controllers/LojaController.php
+++ app/Http/Controllers/LojaController.php
@@ -3,16 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Produto;
+use App\ProdutoAdicional;
+use App\ProdutoTamanho;
+use App\Categoria;
 
 class LojaController extends Controller
 {
-    /**
-     * Página inicial da loja.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('loja');
-    }
+
+	protected $dados = array();
+
+    public function index(){
+
+		$dados['filtrado'] = NULL;
+
+		$dados['categorias'] = Categoria::all()->toArray();
+
+		$produtos = Produto::where('quantidadeEstoque', '>', 0)->get();
+
+		$codigosProdutos = $produtos->pluck('codigoProduto')->toArray();
+
+		$produtosTamanhos = ProdutoTamanho::whereIn('codigoProduto', $codigosProdutos)->get()->toArray();
+
+		$produtosAdicionais = ProdutoAdicional::whereIn('codigoProduto', $codigosProdutos)->get()->toArray();
+
+		$dados['produtos'] = $produtos->toArray();
+
+		$dados['produtosTamanhos'] = $produtosTamanhos->toArray();
+
+		$dados['produtosAdicionais'] = $produtosAdicionais->toArray();
+
+		print_r($produtos);
+
+		//return view('loja', $dados);
+	}
+
+	public function buscarCategoria(Request $request){
+		return 'TODO';
+	}
+
+	public function pesquisarProduto(Request $request){
+		return 'TODO';
+	}
+
+	public function adicionarCarrinho(Request $request) {
+		return 'TODO';
+	}
 }
